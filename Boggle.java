d52a3b866ded59a243a220fa3e2fad987df17bf0
Casey Dinan <cdd43@pitt.edu>
2023-11-01 17:37:45 -0400

    Update Boggle.java

--- Boggle.java
+++ Boggle.java
@@ -8,10 +8,9 @@ public class Boggle
 	static String[][] board;
 	static long startTime,endTime; // for timing
 	static final long MILLISEC_PER_SEC = 1000;
-
-	// define a TreeSet of String named dictionary
+	
 	static TreeSet<String> dict = new TreeSet<String>();
-	// define a TreeSet of String named hits (or matches or wordsFoundInGrid ) only real dictionary words go in here
+
 	static TreeSet<String> hits = new TreeSet<String>();
 
 	public static void main( String args[] ) throws Exception
@@ -19,7 +18,6 @@ public class Boggle
 		startTime= System.currentTimeMillis();
 		board = loadBoard( args[1] );
 
-		// INITIALIZE DICT AND HITS HERE
 		File dictFile = new File(args[0]);
 		Scanner inFile = new Scanner(dictFile);
 		while (inFile.hasNext()) {
@@ -29,8 +27,6 @@ public class Boggle
 			for (int col = 0; col < board[row].length; col++)
 				dfs( row, col, ""  ); // FOR EACH [R][C] THE WORD STARTS EMPTY
 
-		// EVENTUALLY YOU ADD AN ENANCED FOR LOOP HERE
-		// PRINT OUT YOUR HITS ONE WORD PER LINE
 		for (String hit : hits) {
 			System.out.println(hit);
 		}
@@ -45,88 +41,67 @@ public class Boggle
 	{
 		word += board[r][c];
 
-		//System.out.println( word ); // EVENTUALLY REPLACE WITH: IF FOUND IN DICT ADD TO HITS CONTAINER
 		if(word.length() >= 3 && dict.contains(word)) {
 			hits.add(word);
 		}
 		if (word.length() >= 3 && !hasPrefix(word)) {
 			return;
 		}
-		// THE HUERISTIC GOES HERE:  After you search for the word just formed
-		// if the dictionary comes back with false (i.e. not found) then "ask" the dictionary
-		// "are there any words in you that even start with this failed word?"
-		// HOW DO I ASK THE DICTIONARY THIS QUESTION?
-		// (same way you wrote union,intersect,diff with no loops)  READ THE TreeSet API methods!
-		// If the dictionary comes back and says "NO there are no words in me that ever start with that failed word
-		// then you ask yourself " What value is that information?  What should I do if the word I just formed
-		// is not in the dictaionary AND there is not even a word in the dictionary that even starts with this word?
-		// YOU CONNECT THE DOTS AND DECIDE WHAT TO DO IF YOUR FAILED WORD IS NOT A PREFIX OF THE DICTIONARY..
-
-
-		// N O W    Y O U    D O    A    S W E E P    O F   Y O U R   8   N E I G H B O R   C E L L S
-		// L O O K I N G  T O   G R O W   Y O U R   W O R D    W I T H   A    R E C U R S I V E  C A L L
-
-		// IM GIVING you THE NORTH AND NORTHEAST TESTS - YOU MUST WRITE 6 MORE BELOW IT
-		// DO NOT ELSE THEM OFF GIVE EVERY BLOCK AN INDEPENDENT IF TEST
-		// YOU WANT TO RESUME YOUR CLOCKWISE SWEEP OF NEIGHBORS
-
-		// NORTH IS [r-1][c]
-
-		if ( r-1 >= 0   &&   board[r-1][c] != null )   // THE r +/- and c+/- WILL CHANGE FOR EVEY BLOCK BELOW
-		{	String unMarked = board[r][c]; // SAVE TO RESTORE AFTER RET FROM RECURSION
-			board[r][c] = null; // // null IS THE MARKER OF A VALUE AS IN USE ALREADY
-			dfs( r-1, c, word ); // THE r-1,c WILL CHANGE WITH EVERY OTHER BLOCK BELOW
-			board[r][c] = unMarked; // BACK. UNMARK IT
+	
+		if ( r-1 >= 0   &&   board[r-1][c] != null )   
+		{	String unMarked = board[r][c]; 
+			board[r][c] = null; 
+			dfs( r-1, c, word ); 
+			board[r][c] = unMarked; 
 		}
 
-		// NE IS [r-1][c+1]  YOU WILL NEED TO TEST BOTH r-1 AND c+1 FOR OUT OF BOUNDS
 		if ( r-1 >= 0 && c+1 < board[r].length   &&   board[r-1][c+1] != null )
-		{	String unMarked = board[r][c]; // SAVE TO RESTORE AFTER RET FROM RECURSION
-			board[r][c] = null; // // null IS THE MARKER OF A VALUE AS IN USE ALREADY
-			dfs( r-1, c+1, word ); // THE r-1,c+1 etc WILL CHANGE WITH EVERY OTHER BLOCK BELOW
-			board[r][c] = unMarked; // BACK. UNMARK IT
+		{	String unMarked = board[r][c];
+			board[r][c] = null; 
+			dfs( r-1, c+1, word ); 
+			board[r][c] = unMarked; 
 		}
-		// E IS [r][c+1]
+		
 		if (c + 1 < board[r].length && board[r][c+1] != null ) {
-			String unMarked = board[r][c]; // SAVE TO RESTORE AFTER RET FROM RECURSION
-			board[r][c] = null; // // null IS THE MARKER OF A VALUE AS IN USE ALREADY
-			dfs( r, c+1, word ); // THE r-1,c+1 etc WILL CHANGE WITH EVERY OTHER BLOCK BELOW
-			board[r][c] = unMarked; // BACK. UNMARK IT
+			String unMarked = board[r][c]; 
+			board[r][c] = null; 
+			dfs( r, c+1, word ); 
+			board[r][c] = unMarked; 
 		}
-		// SE IS ...
+		
 		if ( r+1 < board[c].length && c+1 < board[r].length   &&   board[r+1][c+1] != null )
-		{	String unMarked = board[r][c]; // SAVE TO RESTORE AFTER RET FROM RECURSION
-			board[r][c] = null; // // null IS THE MARKER OF A VALUE AS IN USE ALREADY
-			dfs( r+1, c+1, word ); // THE r-1,c+1 etc WILL CHANGE WITH EVERY OTHER BLOCK BELOW
-			board[r][c] = unMarked; // BACK. UNMARK IT
+		{	String unMarked = board[r][c]; 
+			board[r][c] = null; 
+			dfs( r+1, c+1, word ); 
+			board[r][c] = unMarked; 
 		}
-		// S IS ...
+		
 		if (r + 1 < board[c].length && board[r+1][c] != null ) {
-			String unMarked = board[r][c]; // SAVE TO RESTORE AFTER RET FROM RECURSION
-			board[r][c] = null; // // null IS THE MARKER OF A VALUE AS IN USE ALREADY
-			dfs( r+1, c, word ); // THE r-1,c+1 etc WILL CHANGE WITH EVERY OTHER BLOCK BELOW
-			board[r][c] = unMarked; // BACK. UNMARK IT
+			String unMarked = board[r][c]; 
+			board[r][c] = null; 
+			dfs( r+1, c, word ); 
+			board[r][c] = unMarked; 
 		}
-		// SW IS ...
+		
 		if ( r+1 < board[c].length && c-1 >= 0   &&   board[r+1][c-1] != null )
-		{	String unMarked = board[r][c]; // SAVE TO RESTORE AFTER RET FROM RECURSION
-			board[r][c] = null; // // null IS THE MARKER OF A VALUE AS IN USE ALREADY
-			dfs( r+1, c-1, word ); // THE r-1,c+1 etc WILL CHANGE WITH EVERY OTHER BLOCK BELOW
-			board[r][c] = unMarked; // BACK. UNMARK IT
+		{	String unMarked = board[r][c]; 
+			board[r][c] = null; 
+			dfs( r+1, c-1, word ); 
+			board[r][c] = unMarked; 
 		}
-		// W IS ...
+		
 		if (c - 1 >= 0 && board[r][c-1] != null ) {
-			String unMarked = board[r][c]; // SAVE TO RESTORE AFTER RET FROM RECURSION
-			board[r][c] = null; // // null IS THE MARKER OF A VALUE AS IN USE ALREADY
-			dfs( r, c-1, word ); // THE r-1,c+1 etc WILL CHANGE WITH EVERY OTHER BLOCK BELOW
-			board[r][c] = unMarked; // BACK. UNMARK IT
+			String unMarked = board[r][c]; 
+			board[r][c] = null; 
+			dfs( r, c-1, word ); 
+			board[r][c] = unMarked; 
 		}
-		// NW IS ...
+		
 		if ( r-1 >= 0 && c-1 >= 0   &&   board[r-1][c-1] != null )
-		{	String unMarked = board[r][c]; // SAVE TO RESTORE AFTER RET FROM RECURSION
-			board[r][c] = null; // // null IS THE MARKER OF A VALUE AS IN USE ALREADY
-			dfs( r-1, c-1, word ); // THE r-1,c+1 etc WILL CHANGE WITH EVERY OTHER BLOCK BELOW
-			board[r][c] = unMarked; // BACK. UNMARK IT
+		{	String unMarked = board[r][c]; 
+			board[r][c] = null; 
+			dfs( r-1, c-1, word ); 
+			board[r][c] = unMarked; 
 		}
 	} // END DFS ----------------------------------------------------------------------------
 
